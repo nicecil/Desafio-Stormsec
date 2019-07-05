f7fbd7152afcf6d2b704895443d472f4d624f9e1
Nickolas Cecilio <nicecil@umich.edu>
2019-07-05 15:28:09 -0300

    Create desafio.py

---
+++ desafio.py
@@ -0,0 +1,9 @@
+class Fato:
+	def __init__(self, nome, atributo, valor, veracidade)
+        self.nome = nome
+        self.atributo = atributo
+        self.valor = valor
+        self.veracidade = veracidade
+    
+    
+	
